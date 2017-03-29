ef98437899b508f8226055d5ec383f39c2702622
BrrltN <barrouillet.nicolas@gmail.com>
2017-03-29 15:18:03 +0200

    correct static controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @cards = Card.all
     @cards_ids = @cards.map{|card| card.id}
-    @first_card = Card.find_by_id((@cards_ids - session[:played_card]).sample)
+    @first_card = Card.find_by_id((@cards_ids).sample)
     session[:budget_gauge] = 50
     session[:employers_gauge] = 50
     session[:population_gauge] = 50
